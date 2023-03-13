2de1027db3f186998fe0570d34341db0c2ccd046
Wonder <64184513+NeverWonderLand@users.noreply.github.com>
2023-03-13 00:52:35 -0400

    Update urlparse.py

--- urlparse.py
+++ urlparse.py
@@ -2,15 +2,19 @@ from urllib.parse import urlparse
 
 # Print banner
 print("*" * 30)
-print("URLsParse - by Wond3r")
+print("URLsParse By Wond3r")
 print("*" * 30)
 
 # Read the list of URLs from a file
 with open("urls.txt", "r") as f:
     url_list = [line.strip() for line in f]
 
-# Loop through each URL
-for url in url_list:
-    # Extract the domain from the URL
-    domain = urlparse(url).netloc
+# Loop through each URL and extract the domain
+domain_list = [urlparse(url).netloc for url in url_list]
+
+# Remove duplicate domains
+unique_domains = list(set(domain_list))
+
+# Print the unique domains
+for domain in unique_domains:
     print(domain)
